80660d5bed1dac46ec9eed1357f94338b2e21e98
Álex Acuña Viera <kyuumeitai@gmail.com>
2020-01-05 22:01:59 -0300

    handler ok

--- backend/opengraphparser/handler.js
+++ backend/opengraphparser/handler.js
@@ -1,10 +1,20 @@
-module.exports = (event, context) => {
-    let err;
-    const result =             {
-        status: "You said: " + JSON.stringify(event.body)
-    };
+const ogs = require('open-graph-scraper')
 
+module.exports = (event, context) => {
+  console.log(event)
+  console.log('queryobj', event.body)
+
+  const options = { 'url': event.body }
+
+  ogs(options).then(function (result) {
     context
-        .status(200)
-        .succeed(result);
+      .status(200)
+      .succeed(result)
+  })
+  .catch(function (error) {
+    context
+      .status(501)
+      .succeed(error)
+  })
+
 }
