88960d6b54ac053a87c68f48e2e1c0d8380a3f0d
Álex Acuña Viera <kyuumeitai@gmail.com>
2020-10-08 14:20:29 -0300

    Header centrado

--- src/components/CustomVideo/Header/style.css.jsx
+++ src/components/CustomVideo/Header/style.css.jsx
@@ -1,13 +1,33 @@
 import styled from "styled-components";
 
 const StHeader = styled.header`
-  background-color: black;
   color: white;
+  /* position: fixed;
+  top: 38px;
+  right: 0;
+  left: 0; */
+  z-index: 2;
+  background-color: black;
   height: 80px;
   padding: 15px 25px;
   display: grid;
-  grid-template-columns: repeat(2, 1fr);
+  grid-template-columns: repeat(3, 1fr);
   align-items: center;
+  pointer-events: none;
+  z-index: 12;
+  @media (max-width: 800px) {
+    grid-template-columns: repeat(2, 1fr);
+  }
+  &:after {
+    content: "";
+    background-image: linear-gradient(rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0));
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    height: 15vh;
+    z-index: -1;
+  }
   div {
     &:first-of-type {
       justify-self: flex-start;
@@ -17,7 +37,7 @@ const StHeader = styled.header`
       }
     }
     &:last-of-type {
-      justify-self: flex-end;
+      justify-self: center;
       display: flex;
       align-items: center;
       img,
@@ -29,6 +49,9 @@ const StHeader = styled.header`
         @media (max-width: 800px) {
           display: none;
         }
+      }
+      @media (max-width: 800px) {
+        justify-self: flex-end;
       }
     }
   }
