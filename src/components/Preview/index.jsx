5df4cfc4b50f11125497236de36416dc308638fa
Álex Acuña Viera <kyuumeitai@gmail.com>
2022-01-14 22:25:51 -0300

    lt sábado

--- src/components/Preview/index.jsx
+++ src/components/Preview/index.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Article from '../Article'
 import Header from '../Header'
-import CustomWidget from '../CustomTheMatchProject/'
+import CustomWidget from '../Custom2022/WidgetLTSabado/'
 import { StWidget, StHeader, StBody } from './style.css'
 
 const Preview = ({ articles, header, site }) => {
