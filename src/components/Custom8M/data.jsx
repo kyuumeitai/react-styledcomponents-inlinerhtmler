649b992ca791f4934e71a6a7c851b8cadd7d9de9
Álex Acuña Viera <kyuumeitai@gmail.com>
2022-03-08 19:16:02 -0300

    asdf

--- src/components/Custom8M/data.jsx
+++ src/components/Custom8M/data.jsx
@@ -5,7 +5,7 @@ const data = [
       'Aquella que a pesar del contexto ha logrado tener un lugar en espacios o rubros que han sido tradicionalmente masculinos. Así, mediante su experiencia personal, profesional y laboral, le ha abierto a otras nuevos caminos y oportunidades.',
     imgurl: 'https://especiales.latercera.com/8m-files/01.png',
     note: {
-      date: '8 Marzo',
+      date: '9 Marzo',
     },
   },
   {
@@ -14,7 +14,7 @@ const data = [
       'La que usa el conocimiento para transformar la vida de otras mujeres. Estudiosa de temas de género, se apropia de espacios como la literatura para contar historias de mujeres y para expresar nuestra visión de la sociedad y así, dar cuenta de nuestras desigualdades. ',
     imgurl: 'https://especiales.latercera.com/8m-files/02.png',
     note: {
-      date: '9 Marzo',
+      date: '10 Marzo',
     },
   },
   {
@@ -23,7 +23,7 @@ const data = [
       'La que históricamente se ha encargado de empoderar comunidades y potenciar las habilidades de otras mujeres. La que organiza ollas comunes o redes de cuidado para que otras puedan salir adelante articulando un tejido social fundamental para estos tiempos.',
     imgurl: 'https://especiales.latercera.com/8m-files/03.png',
     note: {
-      date: '10 Marzo',
+      date: '11 Marzo',
     },
   },
   {
@@ -32,7 +32,7 @@ const data = [
       'La que a través de sus conocimientos sobre el cuerpo nos ayuda a hacer valer nuestros derechos sexuales y reproductivos que por tanto tiempo fueron pasados a llevar. Mujeres que acompañan física y emocionalmente a otras durante el embarazo, el parto y que también nos enseña a sentir placer, a conocer nuestro cuerpo y cuidarlo. ',
     imgurl: 'https://especiales.latercera.com/8m-files/04.png',
     note: {
-      date: '11 Marzo',
+      date: '12 Marzo',
     },
   },
   {
@@ -41,7 +41,7 @@ const data = [
       'Se ha tomado la lucha por una educación integral y no sexista como una lucha personal. Aún teniendo todo un sistema en contra, les enseña a niños y niñas desde la primera infancia, la importancia de la igualdad de género y el respeto por otros y otras. ',
     imgurl: 'https://especiales.latercera.com/8m-files/05.png',
     note: {
-      date: '12 Marzo',
+      date: '13 Marzo',
     },
   },
   {
@@ -50,7 +50,7 @@ const data = [
       'Es la que decide llevar su activismo y vocación al espacio de deliberación en el que se toman las decisiones que determinan la vida de otras mujeres. Es la que actúa en la primera línea, de puente y mediadora recogiendo las demandas sociales y llevándolas a espacios de poder. ',
     imgurl: 'https://especiales.latercera.com/8m-files/06.png',
     note: {
-      date: '13 Marzo',
+      date: '14 Marzo',
     },
   },
   {
@@ -59,7 +59,7 @@ const data = [
       'Alejada de la ciudad, esta mujer trabaja directamente con su comunidad en la búsqueda de mejores condiciones. Tiene una importante conexión con su entorno y su lucha está centrada en las agresiones al medio ambiente que se traducen en una amenaza para su comunidad.',
     imgurl: 'https://especiales.latercera.com/8m-files/07.png',
     note: {
-      date: '14 Marzo',
+      date: '15 Marzo',
     },
   },
   {
@@ -68,7 +68,7 @@ const data = [
       'Esta profesional trabaja sanando o acompañando a otras mujeres. Las ayuda a comprender que son parte de una sociedad y que por tanto sus problemas no son privados, sino públicos. Así las libera de estereotipos desde físicos y sexuales, hasta aquellos que tienen que ver con el rol que debemos cumplir en la sociedad.',
     imgurl: 'https://especiales.latercera.com/8m-files/08.png',
     note: {
-      date: '15 Marzo',
+      date: '16 Marzo',
     },
   },
 ]
