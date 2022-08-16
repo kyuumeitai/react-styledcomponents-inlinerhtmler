e8e2393209c079bba009de727952db7c229a3651
Álex Acuña Viera @ hinata-wsl2 <kyuumeitai@gmail.com>
2022-08-16 09:37:27 -0400

    update

--- src/components/CustomVideoLidera/Data/lidera.jsx
+++ src/components/CustomVideoLidera/Data/lidera.jsx
@@ -16,11 +16,11 @@ const metadata = {
     'https://www.latercera.com/resizer/PdCpQu1erE8K_UpGYS_QKUkSqd0=/arc-anglerfish-arc2-prod-copesa/public/UZUNOYXQOVEIFKQE7AAMDBQEEY.png',
   title: 'Lidera Paula',
   supplemental:
-    '<small>Mujeres, trabajo y nuevos liderazgos</small> <br /><br />Disponible Capítulo 1: Elisa Loncón',
+    '<small>Mujeres, trabajo y nuevos liderazgos</small> <br /><br />Disponible Capítulo 2: Teresa Matamala',
 
   synopsis:
     'Un nuevo espacio de conversación que busca conocer las claves detrás del liderazgo femenino y se propone indagar en los desafíos que existen en materia de género y trabajo, tales como la corresponsabilidad, alternativas al sistema actual de cuidados, el techo de cristal y mujeres en la ciencia.',
-  mainlink: 'https://www.latercera.com/lidera/01-elisa-loncon/',
+  mainlink: 'https://www.latercera.com/lidera/02-teresa-matamala/',
   secondarylink:
     'https://www.latercera.com/paula/la-tercera-y-revista-paula-presentan-lidera/?utm_source=widgetLT&utm_medium=web&utm_campaign=lidera-paula',
   btntext: 'Reproducir',
@@ -50,18 +50,23 @@ const data = [
     extendedVideo: 'https://www.youtube.com/watch?v=l61N1US2Bvk',
   },
   {
+    url: 'https://www.latercera.com/lidera/02-teresa-matamala/',
     img:
       'https://www.latercera.com/resizer/lL-9Rm35GHokl1mxR6ZdCKoJqf4=/arc-anglerfish-arc2-prod-copesa/public/IPCK7GOVTJFJNF7ZUBWLP7UQLM.jpg',
-    status: ['soon'],
+    status: ['available', 'new'],
     pretitle: 'Capítulo 02',
     name: 'Teresa Matamala',
     aftername:
       'Experta en asuntos corporativos y sostenibilidad y socia de REDMAD',
     title: 'Mujeres y el avance de sus carreras profesionales',
     preview:
-      'https://ads-lt.digitalproserver.com/ads/lab/lidera-paula/prev-cap00-comp.mp4',
-    label: 'Pronto',
-    description: 'El avance de las carreras profesionales de las mujeres',
+      'https://ads-lt.digitalproserver.com/ads/lab/lidera-paula/prev-cap02-comp.mp4',
+    label: 'Nuevo',
+    description: `<p>En esta conversación, la experta en asuntos corporativos y sostenibilidad, y socia fundadora de REDMAD, Teresa Matamala, habla sobre las brechas de género en el mundo empresarial y explica por qué encontramos a menos mujeres en la medida que vamos subiendo de rango en las organizaciones. </p>
+
+    <p>Según explica, los puestos de alta dirección implican una alta demanda de tiempo, que -por cómo concebimos los cuidados hoy- muchas mujeres no tienen. Pero la pandemia nos ha obligado a revisar estas desigualdades, así como buscar alternativas que favorezcan la inclusión y desarrollo profesional. ¿Qué hay que hacer para cambiar esta realidad? ¿Qué políticas se tienen que impulsar desde el sector privado para asegurar una equidad en materia de género? ¿Cómo se articulan el sector público y privado para encontrar soluciones a corto plazo?</p>`,
+    iframe: 'https://rudo.video/vod/bOvquc',
+    extendedVideo: 'https://youtu.be/ku2CB1EFsYM',
   },
   {
     img:
--- src/components/CustomVideoLidera/Single/index.jsx
+++ src/components/CustomVideoLidera/Single/index.jsx
@@ -8,7 +8,7 @@ import Footer from '../Footer'
 import { data, metadata } from '../Data/lidera'
 
 const VideoHome = () => {
-  const chapter = data[10]
+  const chapter = data[1]
 
   return (
     <Wrap>
