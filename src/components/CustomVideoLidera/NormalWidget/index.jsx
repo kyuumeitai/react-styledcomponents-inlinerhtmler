2134835dc8158de0fcbe9a5e4c9d38cae21d5866
Álex Acuña Viera @NatakuWSL2 <kyuumeitai@gmail.com>
2022-09-13 10:13:42 -0300

    widget

--- src/components/CustomVideoLidera/NormalWidget/index.jsx
+++ src/components/CustomVideoLidera/NormalWidget/index.jsx
@@ -6,15 +6,14 @@ const videodesktop = `https://ads-lt.digitalproserver.com/ads/lab/lidera-paula/l
 const image = `https://www.latercera.com/resizer/9-xENSLfEN7gvnVcUFahEoofPyg=/arc-anglerfish-arc2-prod-copesa/public/DGULDB6IPBHVJIXCAXYG25PN54.jpg`
 
 const metadata = {
-  logo:
-    'https://www.latercera.com/resizer/PdCpQu1erE8K_UpGYS_QKUkSqd0=/arc-anglerfish-arc2-prod-copesa/public/UZUNOYXQOVEIFKQE7AAMDBQEEY.png',
+  logo: 'https://www.latercera.com/resizer/PdCpQu1erE8K_UpGYS_QKUkSqd0=/arc-anglerfish-arc2-prod-copesa/public/UZUNOYXQOVEIFKQE7AAMDBQEEY.png',
   title: 'Lidera Paula',
   supplemental:
-    '<small>Mujeres, trabajo y nuevos liderazgos</small> <br />Capítulo 6: Alessia Injoque',
+    '<small>Mujeres, trabajo y nuevos liderazgos</small> <br />Capítulo 7: Marcela Bravo',
   synopsis:
     'Un nuevo espacio de conversación que busca conocer las claves detrás del liderazgo femenino y se propone indagar en los desafíos que existen en materia de género y trabajo.',
   mainlink:
-    'https://www.latercera.com/lidera/06-alessia-injoque/?utm_source=widgetLT&utm_medium=web&utm_campaign=lidera-paula',
+    'https://www.latercera.com/lidera/07-marcela-bravo/?utm_source=widgetLT&utm_medium=web&utm_campaign=lidera-paula',
 
   btntext: 'Reproducir',
 }
