2193c6879fec1acc192baa3e43270d4811b7f4d4
Álex Acuña Viera @NatakuWSL2 <kyuumeitai@gmail.com>
2022-09-23 09:49:14 -0300

    lidera

--- src/components/CustomVideoLidera/Data/lidera.jsx
+++ src/components/CustomVideoLidera/Data/lidera.jsx
@@ -12,15 +12,14 @@ const logoproductor =
   'https://www.latercera.com/resizer/kXPp5UxoLulsiUCHx6GjdFCGC5s=/arc-anglerfish-arc2-prod-copesa/public/YE4IYFI4NNCD7JV4EMCR3OPPBI.png'
 
 const metadata = {
-  logo:
-    'https://www.latercera.com/resizer/PdCpQu1erE8K_UpGYS_QKUkSqd0=/arc-anglerfish-arc2-prod-copesa/public/UZUNOYXQOVEIFKQE7AAMDBQEEY.png',
+  logo: 'https://www.latercera.com/resizer/PdCpQu1erE8K_UpGYS_QKUkSqd0=/arc-anglerfish-arc2-prod-copesa/public/UZUNOYXQOVEIFKQE7AAMDBQEEY.png',
   title: 'Lidera Paula',
   supplemental:
-    '<small>Mujeres, trabajo y nuevos liderazgos</small> <br /><br />Disponible Capítulo 7: <br />Marcela Bravo',
+    '<small>Mujeres, trabajo y nuevos liderazgos</small> <br /><br />Disponible Capítulo 8: <br />Maribel Vidal',
 
   synopsis:
     'Un nuevo espacio de conversación que busca conocer las claves detrás del liderazgo femenino y se propone indagar en los desafíos que existen en materia de género y trabajo, tales como la corresponsabilidad, alternativas al sistema actual de cuidados, el techo de cristal y mujeres en la ciencia.',
-  mainlink: 'https://www.latercera.com/lidera/07-marcela-bravo/',
+  mainlink: 'https://www.latercera.com/lidera/08-maribel-vidal/',
   secondarylink:
     'https://www.latercera.com/paula/la-tercera-y-revista-paula-presentan-lidera/?utm_source=widgetLT&utm_medium=web&utm_campaign=lidera-paula',
   btntext: 'Reproducir',
@@ -30,8 +29,7 @@ const metadata = {
 const data = [
   {
     url: 'https://www.latercera.com/lidera/01-elisa-loncon/',
-    img:
-      'https://www.latercera.com/resizer/0fTiuYpRKuCnSm073o62mZeAnTU=/arc-anglerfish-arc2-prod-copesa/public/KDYWMTR2CRFDLIETKH74WFCWQA.jpg',
+    img: 'https://www.latercera.com/resizer/0fTiuYpRKuCnSm073o62mZeAnTU=/arc-anglerfish-arc2-prod-copesa/public/KDYWMTR2CRFDLIETKH74WFCWQA.jpg',
     status: ['new', 'available'],
     pretitle: 'Capítulo 01',
     name: 'Elisa Loncón',
@@ -51,8 +49,7 @@ const data = [
   },
   {
     url: 'https://www.latercera.com/lidera/02-teresa-matamala/',
-    img:
-      'https://www.latercera.com/resizer/lL-9Rm35GHokl1mxR6ZdCKoJqf4=/arc-anglerfish-arc2-prod-copesa/public/IPCK7GOVTJFJNF7ZUBWLP7UQLM.jpg',
+    img: 'https://www.latercera.com/resizer/lL-9Rm35GHokl1mxR6ZdCKoJqf4=/arc-anglerfish-arc2-prod-copesa/public/IPCK7GOVTJFJNF7ZUBWLP7UQLM.jpg',
     status: ['available', 'new'],
     pretitle: 'Capítulo 02',
     name: 'Teresa Matamala',
@@ -70,8 +67,7 @@ const data = [
   },
   {
     url: 'https://www.latercera.com/lidera/03-vania-figueroa/',
-    img:
-      'https://www.latercera.com/resizer/9TbKGLbbFxxiGiE7JnhQzBsylkM=/arc-anglerfish-arc2-prod-copesa/public/QLLDPV2IGZCEXGH5M6GAALS7OU.jpg',
+    img: 'https://www.latercera.com/resizer/9TbKGLbbFxxiGiE7JnhQzBsylkM=/arc-anglerfish-arc2-prod-copesa/public/QLLDPV2IGZCEXGH5M6GAALS7OU.jpg',
     status: ['available', 'new'],
     pretitle: 'Capítulo 03',
     name: 'Vania Figueroa',
@@ -87,8 +83,7 @@ const data = [
   },
   {
     url: 'https://www.latercera.com/lidera/04-claudia-bobadilla/',
-    img:
-      'https://www.latercera.com/resizer/-vxpYBd8iNPPcGIMH_tiEuwe7j0=/arc-anglerfish-arc2-prod-copesa/public/2RKAZU2ATNF7DEVAJP2GNWI7TA.jpg',
+    img: 'https://www.latercera.com/resizer/-vxpYBd8iNPPcGIMH_tiEuwe7j0=/arc-anglerfish-arc2-prod-copesa/public/2RKAZU2ATNF7DEVAJP2GNWI7TA.jpg',
     status: ['available', 'new'],
     pretitle: 'Capítulo 04',
     name: 'Claudia Bobadilla',
@@ -105,8 +100,7 @@ const data = [
 
   {
     url: 'https://www.latercera.com/lidera/05-laura-perez/',
-    img:
-      'https://www.latercera.com/resizer/ttD3DRuqHvbS4OshCSz1aNJ-Mwc=/arc-anglerfish-arc2-prod-copesa/public/RBWJPMBHXJBHLDJL2IYSHBNEGU.jpg',
+    img: 'https://www.latercera.com/resizer/ttD3DRuqHvbS4OshCSz1aNJ-Mwc=/arc-anglerfish-arc2-prod-copesa/public/RBWJPMBHXJBHLDJL2IYSHBNEGU.jpg',
     status: ['new', 'available'],
     pretitle: 'Capítulo 05',
     name: 'Laura Pérez',
@@ -122,8 +116,7 @@ const data = [
   },
   {
     url: 'https://www.latercera.com/lidera/06-alessia-injoque/',
-    img:
-      'https://www.latercera.com/resizer/v7GWYIa48ep12KSCW9NMj4qNX6k=/arc-anglerfish-arc2-prod-copesa/public/ZVHCME6WIFBOBM4PE72PMCZVGI.jpg',
+    img: 'https://www.latercera.com/resizer/v7GWYIa48ep12KSCW9NMj4qNX6k=/arc-anglerfish-arc2-prod-copesa/public/ZVHCME6WIFBOBM4PE72PMCZVGI.jpg',
     status: ['new', 'available'],
     pretitle: 'Capítulo 06',
     name: 'Alessia Injoque',
@@ -140,8 +133,7 @@ const data = [
   {
     url: 'https://www.latercera.com/lidera/07-marcela-bravo/',
 
-    img:
-      'https://www.latercera.com/resizer/BjjMfW4NW3vNhZrV-YJVfipyWhw=/arc-anglerfish-arc2-prod-copesa/public/X2FDT2XI2REQLEWOKNFN65CZMI.jpg',
+    img: 'https://www.latercera.com/resizer/BjjMfW4NW3vNhZrV-YJVfipyWhw=/arc-anglerfish-arc2-prod-copesa/public/X2FDT2XI2REQLEWOKNFN65CZMI.jpg',
     status: ['new', 'available'],
     pretitle: 'Capítulo 07',
     name: 'Marcela Bravo',
@@ -158,8 +150,7 @@ const data = [
 
   {
     url: 'https://www.latercera.com/lidera/08-maribel-vidal/',
-    img:
-      'https://www.latercera.com/resizer/yjWZFvDNEjeGv8aX_ZDmj226wIw=/arc-anglerfish-arc2-prod-copesa/public/FN4LZI2ZYFBQDEFLC3DHH7MOUA.jpg',
+    img: 'https://www.latercera.com/resizer/yjWZFvDNEjeGv8aX_ZDmj226wIw=/arc-anglerfish-arc2-prod-copesa/public/FN4LZI2ZYFBQDEFLC3DHH7MOUA.jpg',
     status: ['new', 'available'],
     pretitle: 'Capítulo 08',
     name: 'Maribel Vidal',
@@ -174,8 +165,7 @@ const data = [
     extendedVideo: 'https://youtu.be/iE12wJdMmxc',
   },
   {
-    img:
-      'https://www.latercera.com/resizer/wZ9Jy0Q5T2OgbrvTuWJIWeYnTjg=/arc-anglerfish-arc2-prod-copesa/public/WVDVEI4L75DVREU5KMET3R3OKY.jpg',
+    img: 'https://www.latercera.com/resizer/wZ9Jy0Q5T2OgbrvTuWJIWeYnTjg=/arc-anglerfish-arc2-prod-copesa/public/WVDVEI4L75DVREU5KMET3R3OKY.jpg',
     status: ['soon'],
     pretitle: 'Capítulo 09',
     name: 'Pilar Velasco',
@@ -187,8 +177,7 @@ const data = [
     description: 'Las políticas de género en una empresa B y el emprendimiento',
   },
   {
-    img:
-      'https://www.latercera.com/resizer/-wiA-L5zWrgcOj9IDSf8pdNL3RQ=/arc-anglerfish-arc2-prod-copesa/public/WATBI3KOZ5EWZOM7QB24ZFIRLI.jpg',
+    img: 'https://www.latercera.com/resizer/-wiA-L5zWrgcOj9IDSf8pdNL3RQ=/arc-anglerfish-arc2-prod-copesa/public/WATBI3KOZ5EWZOM7QB24ZFIRLI.jpg',
     status: ['soon'],
     pretitle: 'Capítulo 10',
     name: 'María Ana Matthias',
@@ -202,8 +191,7 @@ const data = [
 
   {
     url: 'https://www.latercera.com/lidera/00-teaser/',
-    img:
-      'https://www.latercera.com/resizer/r53hnahsLf8U91vabXvkYd1PlvM=/arc-anglerfish-arc2-prod-copesa/public/ODA7RTTR4JFUJOEEK6M27OZXQQ.jpg',
+    img: 'https://www.latercera.com/resizer/r53hnahsLf8U91vabXvkYd1PlvM=/arc-anglerfish-arc2-prod-copesa/public/ODA7RTTR4JFUJOEEK6M27OZXQQ.jpg',
     status: ['new', 'available'],
     pretitle: 'Capítulo 00',
     name: 'Teaser',
--- src/components/CustomVideoLidera/NormalWidget/index.jsx
+++ src/components/CustomVideoLidera/NormalWidget/index.jsx
@@ -9,11 +9,11 @@ const metadata = {
   logo: 'https://www.latercera.com/resizer/PdCpQu1erE8K_UpGYS_QKUkSqd0=/arc-anglerfish-arc2-prod-copesa/public/UZUNOYXQOVEIFKQE7AAMDBQEEY.png',
   title: 'Lidera Paula',
   supplemental:
-    '<small>Mujeres, trabajo y nuevos liderazgos</small> <br />Capítulo 7: Marcela Bravo',
+    '<small>Mujeres, trabajo y nuevos liderazgos</small> <br />Capítulo 8: Maribel Vidal',
   synopsis:
     'Un nuevo espacio de conversación que busca conocer las claves detrás del liderazgo femenino y se propone indagar en los desafíos que existen en materia de género y trabajo.',
   mainlink:
-    'https://www.latercera.com/lidera/07-marcela-bravo/?utm_source=widgetLT&utm_medium=web&utm_campaign=lidera-paula',
+    'https://www.latercera.com/lidera/08-maribel-vidal/?utm_source=widgetLT&utm_medium=web&utm_campaign=lidera-paula',
 
   btntext: 'Reproducir',
 }
