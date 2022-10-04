df1acfbbe0969c1ad6ac5ece597d09f7365111f8
Ålex Acuña Viera <kyuumeitai@gmail.com>
2022-10-04 10:54:47 -0300

    cap 10 final lidera

--- src/components/CustomVideoLidera/Data/lidera.jsx
+++ src/components/CustomVideoLidera/Data/lidera.jsx
@@ -15,11 +15,11 @@ const metadata = {
   logo: 'https://www.latercera.com/resizer/PdCpQu1erE8K_UpGYS_QKUkSqd0=/arc-anglerfish-arc2-prod-copesa/public/UZUNOYXQOVEIFKQE7AAMDBQEEY.png',
   title: 'Lidera Paula',
   supplemental:
-    '<small>Mujeres, trabajo y nuevos liderazgos</small> <br /><br />Disponible Capítulo 9: <br />Pilar Velasco',
+    '<small>Mujeres, trabajo y nuevos liderazgos</small> <br /><br />Disponible Capítulo 10: <br />María Ana Matthias',
 
   synopsis:
     'Un nuevo espacio de conversación que busca conocer las claves detrás del liderazgo femenino y se propone indagar en los desafíos que existen en materia de género y trabajo, tales como la corresponsabilidad, alternativas al sistema actual de cuidados, el techo de cristal y mujeres en la ciencia.',
-  mainlink: 'https://www.latercera.com/lidera/09-pilar-velasco/',
+  mainlink: 'https://www.latercera.com/lidera/10-maria-ana-matthias/',
   secondarylink:
     'https://www.latercera.com/paula/la-tercera-y-revista-paula-presentan-lidera/?utm_source=widgetLT&utm_medium=web&utm_campaign=lidera-paula',
   btntext: 'Reproducir',
@@ -38,7 +38,7 @@ const data = [
     title: 'Interculturalidad, plurinacionalidad y nuevos liderazgos',
     preview:
       'https://ads-lt.digitalproserver.com/ads/lab/lidera-paula/prev-cap01-comp.mp4',
-    label: 'Nuevo',
+    label: 'Disponible',
     description: `<p>En esta conversación íntima, la lingüista y expresidenta de la Convención Constitucional, Elisa Loncón, da cuenta de su experiencia liderando el proceso Constituyente y las múltiples instancias de discriminación a las que se vio enfrentada. Se abordan temas como el liderazgo desde la ternura y la humanidad; y la importancia de contar con la plurinacionalidad en la nueva Constitución del país. Además, apunta al diálogo desde la interculturalidad para abordar la problemática en la Macrozona sur.</p> 
     
     <p>“Los nuevos liderazgos tienen que priorizar la humanidad, una apertura a lo distinto y ser interculturales. Y necesitamos un empresariado que considere los derechos de la naturaleza y los derechos fundamentales de las personas”, cierra. </p>
@@ -58,7 +58,7 @@ const data = [
     title: 'Mujeres y el avance de sus carreras profesionales',
     preview:
       'https://ads-lt.digitalproserver.com/ads/lab/lidera-paula/prev-cap02-comp.mp4',
-    label: 'Nuevo',
+    label: 'Disponible',
     description: `<p>En esta conversación, la experta en asuntos corporativos y sostenibilidad, y socia fundadora de REDMAD, Teresa Matamala, habla sobre las brechas de género en el mundo empresarial y explica por qué encontramos a menos mujeres en la medida que vamos subiendo de rango en las organizaciones. </p>
 
     <p>Según explica, los puestos de alta dirección implican una alta demanda de tiempo, que -por cómo concebimos los cuidados hoy- muchas mujeres no tienen. Pero la pandemia nos ha obligado a revisar estas desigualdades, así como buscar alternativas que favorezcan la inclusión y desarrollo profesional. ¿Qué hay que hacer para cambiar esta realidad? ¿Qué políticas se tienen que impulsar desde el sector privado para asegurar una equidad en materia de género? ¿Cómo se articulan el sector público y privado para encontrar soluciones a corto plazo?</p>`,
@@ -75,7 +75,7 @@ const data = [
     title: 'Neurociencia con perspectiva de género',
     preview:
       'https://ads-lt.digitalproserver.com/ads/lab/lidera-paula/prev-cap03-comp.mp4',
-    label: 'Nuevo',
+    label: 'Disponible',
     description:
       '<p>En este capítulo, la doctora en neurociencia, Vania Figueroa, cuenta que a lo largo de la historia, hemos dado paso a una cultura dentro de la investigación científica que omite por completo el cuerpo femenino. Así mismo, se han validado mitos: “No existe el "cerebro masculino" y el "cerebro femenino", salvo por una leve diferencia en el tamaño, pero es a partir de estas ideas que se generan consecuencias sociales y políticas”, sostiene. Y es que, como profundiza, menos de un 30% de los investigadores a nivel mundial son mujeres. En Chile, apenas un 31% de las investigadoras cuenta con un doctorado. “A eso le llamamos "la tubería con fuga", porque en la medida que vamos subiendo de rango, no encontramos a mujeres en la ciencia. Pero esa fuga no es pasiva, se da gracias a un entorno que expulsa a las mujeres de la academia”, termina.</p> ',
     iframe: 'https://rudo.video/vod/bOxCte',
@@ -91,7 +91,7 @@ const data = [
     title: 'Fomentar la corresponsabilidad desde el sector privado',
     preview:
       'https://ads-lt.digitalproserver.com/ads/lab/lidera-paula/prev-cap00-comp.mp4',
-    label: 'Nuevo',
+    label: 'Disponible',
     description:
       '<p>En el cuarto capítulo de Lidera, conversamos con la directora de empresas y fundadora de Puente Social, Claudia Bobadilla, sobre corresponsabilidad, la crisis de los cuidados y de qué manera el sector privado puede y debe aportar mejorando las condiciones laborales para que todos los integrantes del núcleo familiar se involucren en la crianza. Además, la especialista dio cuenta de la falta de participación de los hombres en el postnatal parental desde la promulgación de la Ley. Revisa la versión completa en YouTube.</p>',
     iframe: 'https://rudo.video/vod/bO0ukU',
@@ -108,7 +108,7 @@ const data = [
     title: 'Cómo descentralizar la astronomía',
     preview:
       'https://ads-lt.digitalproserver.com/ads/lab/lidera-paula/prev-cap00-comp.mp4',
-    label: 'Nuevo',
+    label: 'Disponible',
     description:
       '<p>La astrónoma y académica del departamento de Astronomía de la Universidad de Chile, Laura Pérez, conversó con la periodista Emiliana Pariente sobre cómo integrar la ciencia al relato social, el rol de la academia en acercar esta disciplina a la ciudadanía y la importancia de tener referentes para incentivar el desarrollo integral de estos rubros. Además, habló sobre los fenómenos astronómicos recientes, la descentralización de la ciencia y la importancia de la colaboración en el mundo de la investigación.</p>',
     iframe: 'https://rudo.video/vod/bO2H3e',
@@ -124,7 +124,7 @@ const data = [
     title: 'Liderazgos inclusivos y diversos',
     preview:
       'https://ads-lt.digitalproserver.com/ads/lab/lidera-paula/prev-cap00-comp.mp4',
-    label: 'Nuevo',
+    label: 'Disponible',
     description:
       'En el sexto capítulo de Lidera, conversamos con la líder de equipos y proyectos en Consultora SAP y activista trans, Alessia Injoque, sobre cómo fue vivir su tránsito en un cargo de liderazgo en el Chile del 2017; sobre la discriminación por las apariencias físicas que se sigue viviendo en los entornos laborales; el valor de la diversidad dentro de las organizaciones y por qué es importante potenciar, especialmente en tiempos de cambio, los liderazgos inclusivos y diversos, algo que ella misma cataloga como una decisión ética pero también estratégica e inteligente: “Se ha podido corroborar que la diversidad en los equipos mejora la competitividad, la creatividad y el clima laboral”, desarrolla.',
     iframe: 'https://rudo.video/vod/bO5rOf',
@@ -141,7 +141,7 @@ const data = [
     title: 'Entender la sostenibilidad desde las empresas',
     preview:
       'https://ads-lt.digitalproserver.com/ads/lab/lidera-paula/prev-cap00-comp.mp4',
-    label: 'Nuevo',
+    label: 'Disponible',
     description:
       'En este capítulo conversamos con la ingeniera comercial y gerente general de Acción Empresas, Marcela Bravo, sobre la importancia de la sostenibilidad en las organizaciones, entendida como el nuevo paradigma que reemplaza a la tradicional Responsabilidad Social Empresarial (RSE). En conversación con la periodista Emiliana Pariente, Bravo analizó los nuevos liderazgos y detalló cómo el estallido social tuvo un impacto en el relacionamiento interno de las empresas. “Cuando las organizaciones se dieron cuenta que muchos de los que iban a protestar eran sus colaboradores, empezaron a querer entablar diálogos con las comunidades. Pasamos de creer que con generar trabajo o un producto bueno era suficiente, a entender que las empresas son ciudadanos corporativos que deben involucrarse y dialogar para entender la realidad”, señala.',
     iframe: 'https://rudo.video/vod/bO6Hrl',
@@ -158,7 +158,7 @@ const data = [
     title: 'Claves para el liderazgo del futuro y la flexibilidad laboral',
     preview:
       'https://ads-lt.digitalproserver.com/ads/lab/lidera-paula/prev-cap00-comp.mp4',
-    label: 'Nuevo',
+    label: 'Disponible',
     description:
       'En este capítulo, la vicepresidenta de McCann y de REDMAD, Maribel Vidal, rescata la importancia de los liderazgos modernos, horizontales y que son capaces de generar espacios de confianza, especialmente en momentos de crisis. Es enfática al decir que las habilidades y características de liderazgo son agnósticas de género y que lo que distingue a un buen liderazgo es la capacidad de leer el entorno, darse el tiempo para escuchar y, sobre todo, cuestionar constantemente los propios sesgos. Los desafíos para los líderes de aquí en adelante tienen que ver, como explica, con lograr generar políticas que resguarden la flexibilidad laboral y el trabajo híbrido basado en la confianza. “Tenemos que ser capaces de escuchar todas las voces para encontrar la mejor solución”, dice.',
     iframe: 'https://rudo.video/vod/bO6Hww',
@@ -174,23 +174,45 @@ const data = [
     title: 'Políticas de género en una empresa B',
     preview:
       'https://ads-lt.digitalproserver.com/ads/lab/lidera-paula/prev-cap00-comp.mp4',
-    label: 'Nuevo',
+    label: 'Disponible',
     description:
       'En este capítulo, la periodista y socia directora de Extend Comunicaciones, Pilar Velasco, cuenta cómo fue fundar una agencia de comunicación estratégica en los años 90 y de qué manera ha cambiado el escenario de lo que implica emprender y trabajar en una empresa hoy. A su vez, reflexiona sobre por qué es fundamental diversificar los equipos para ser sostenibles en el tiempo; sobre el emprendimiento y la confianza; y la importancia de incorporar políticas de género en una empresa B.',
     iframe: 'https://rudo.video/vod/bOCRD2',
     extendedVideo: 'https://youtu.be/MSu3aKKy6BU',
   },
   {
+    url: 'https://www.latercera.com/lidera/10-maria-ana-matthias/',
     img: 'https://www.latercera.com/resizer/-wiA-L5zWrgcOj9IDSf8pdNL3RQ=/arc-anglerfish-arc2-prod-copesa/public/WATBI3KOZ5EWZOM7QB24ZFIRLI.jpg',
-    status: ['soon'],
+    status: ['new', 'available'],
     pretitle: 'Capítulo 10',
     name: 'María Ana Matthias',
     aftername: 'Directora de empresas y de REDMAD',
     title: 'Teletrabajo y corresponsabilidad',
     preview:
       'https://ads-lt.digitalproserver.com/ads/lab/lidera-paula/prev-cap00-comp.mp4',
-    label: 'Pronto',
-    description: 'Teletrabajo y corresponsabilidad',
+    label: 'Nuevo',
+    description: `En este capítulo, la directora de empresas y de REDMAD, María Ana
+    Matthias, reflexiona respecto a los beneficios y desafíos que se abren
+    en el escenario de la flexibilidad laboral, un formato que ya se había
+    puesto en práctica en empresas de Estados Unidos previo al 2020 pero
+    cuya masificación se aceleró en la pandemia. A su vez, habla de que si
+    bien la mayoría de las personas está satisfecha con esta posibilidad,
+    lo que hay que abordar tiene que ver con el sistema actual de
+    cuidados, que recae mayoritariamente en las mujeres y que hizo que
+    fueran ellas las que tuvieran que reformular sus actividades, muchas
+    veces trabajando el doble. Un estudio realizado por REDMAD así lo
+    demostró; las ejecutivas de empresas del país declararon que estaban
+    trabajando más horas de lo habitual y en distintos momentos del día.
+    Desde el sector privado, aclara Matthias, hay que hacerse cargo;
+    generar protocolos para garantizar el derecho a la desconexión, evitar
+    que los colaboradores estén trabajando muchas horas, incentivar que
+    más hombres se tomen el derecho al postnatal y naturalizar que los
+    nuevos liderazgos sean horizontales y generen espacios de confianza.
+    “La barrera entre lo laboral y lo privado se perdió. Eso tiene
+    beneficios pero tiene muchos costos también, especialmente para las
+    mujeres”, señala.`,
+    iframe: 'https://rudo.video/vod/bOFx2R',
+    extendedVideo: 'https://youtu.be/k2RCultBgWk',
   },
 
   {
@@ -204,7 +226,7 @@ const data = [
       'Lidera, el nuevo ciclo de conversaciones de La Tercera y Revista Paula',
     preview:
       'https://ads-lt.digitalproserver.com/ads/lab/lidera-paula/prev-cap00-comp.mp4',
-    label: 'Nuevo',
+    label: 'Disponible',
     description:
       'Lidera, el nuevo ciclo de conversaciones de La Tercera y Revista Paula, busca indagar en las amplias temáticas y desafíos que giran en torno a las mujeres y el trabajo remunerado y no remunerado. A través de la mirada de 10 especialistas tanto del mundo público como privado, se exponen temas de relevancia social, de los cuales mucho se ha hablado, aunque en pocas instancias se han abordado con una perspectiva integral y de género. Entre ellos, los nuevos liderazgos, la inclusión de mujeres en la alta dirección y en la ciencia, la corresponsabilidad parental, las alternativas al sistema actual de cuidados y de qué manera el sector privado puede y debe aportar al desarrollo de las condiciones equitativas entre hombres y mujeres.',
     iframe: 'https://rudo.video/vod/bObAfb',
