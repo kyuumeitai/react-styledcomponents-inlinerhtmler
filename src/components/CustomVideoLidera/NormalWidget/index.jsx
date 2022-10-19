977c50edcae23255314b46fa175a32cc925e4cc3
Ålex Acuña Viera <kyuumeitai@gmail.com>
2022-10-19 15:22:47 -0300

    normalwidget

--- src/components/CustomVideoLidera/NormalWidget/index.jsx
+++ src/components/CustomVideoLidera/NormalWidget/index.jsx
@@ -9,11 +9,11 @@ const metadata = {
   logo: 'https://www.latercera.com/resizer/PdCpQu1erE8K_UpGYS_QKUkSqd0=/arc-anglerfish-arc2-prod-copesa/public/UZUNOYXQOVEIFKQE7AAMDBQEEY.png',
   title: 'Lidera Paula',
   supplemental:
-    '<small>Mujeres, trabajo y nuevos liderazgos</small> <br />Capítulo 10: María Ana Matthias',
+    '<small>Mujeres, trabajo y nuevos liderazgos</small> <br />Capítulo 11: Bárbara Swears',
   synopsis:
     'Un nuevo espacio de conversación que busca conocer las claves detrás del liderazgo femenino y se propone indagar en los desafíos que existen en materia de género y trabajo.',
   mainlink:
-    'https://www.latercera.com/lidera/10-maria-ana-matthias/?utm_source=widgetLT&utm_medium=web&utm_campaign=lidera-paula',
+    'https://www.latercera.com/lidera/11-barbara-swears/?utm_source=widgetLT&utm_medium=web&utm_campaign=lidera-paula',
 
   btntext: 'Reproducir',
 }
