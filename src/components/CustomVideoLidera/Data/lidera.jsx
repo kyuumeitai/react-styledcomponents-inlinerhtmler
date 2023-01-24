4e2c4006b19080bc88939bc39d707b6c20fe1c90
Ålex Acuña Viera <kyuumeitai@gmail.com>
2023-01-24 11:05:36 -0300

    (chore) update widgets on lidera

--- src/components/CustomVideoLidera/Data/lidera.jsx
+++ src/components/CustomVideoLidera/Data/lidera.jsx
@@ -15,11 +15,11 @@ const metadata = {
   logo: 'https://www.latercera.com/resizer/PdCpQu1erE8K_UpGYS_QKUkSqd0=/arc-anglerfish-arc2-prod-copesa/public/UZUNOYXQOVEIFKQE7AAMDBQEEY.png',
   title: 'Lidera Paula',
   supplemental:
-    '<small>Mujeres, trabajo y nuevos liderazgos</small> <br /><br />Disponible Capítulo 19: <br />Valentina Muñoz',
+    '<small>Mujeres, trabajo y nuevos liderazgos</small> <br /><br />Disponible Capítulo 20: <br />Alejandra Matus',
 
   synopsis:
     'Un nuevo espacio de conversación que busca conocer las claves detrás del liderazgo femenino y se propone indagar en los desafíos que existen en materia de género y trabajo, tales como la corresponsabilidad, alternativas al sistema actual de cuidados, el techo de cristal y mujeres en la ciencia.',
-  mainlink: 'https://www.latercera.com/lidera/19-valentina-munoz/',
+  mainlink: 'https://www.latercera.com/lidera/20-alejandra-matus/',
   secondarylink:
     'https://www.latercera.com/paula/la-tercera-y-revista-paula-presentan-lidera/?utm_source=widgetLT&utm_medium=web&utm_campaign=lidera-paula',
   btntext: 'Reproducir',
@@ -343,7 +343,7 @@ const data = [
   {
     url: 'https://www.latercera.com/lidera/19-valentina-munoz/',
     img: 'https://www.latercera.com/resizer/4OV_ZiysfBCzD-wnldeVQ-v2-40=/arc-anglerfish-arc2-prod-copesa/public/6ECLTJULSFHDBBILZ5442YWARA.jpg',
-    status: ['available'],
+    status: ['new', 'available'],
     pretitle: 'Capítulo 19',
     name: 'Valentina Muñoz',
     aftername:
@@ -362,7 +362,7 @@ const data = [
     status: ['new', 'available'],
     pretitle: 'Capítulo 20',
     name: 'Alejandra Matus',
-    aftername: 'Periodista',
+    aftername: 'Periodista, escritora y fundadora de La Neta',
     title: 'Nuevos medios y perspectiva de género en el periodismo',
     preview:
       'https://ads-lt.digitalproserver.com/ads/lab/lidera-paula/prev-cap00-comp.mp4',
