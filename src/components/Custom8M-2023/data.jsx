1ca0042a7ac993c950f850e818cd1abf3cee246a
Ålex Acuña Viera <kyuumeitai@gmail.com>
2023-03-08 10:58:59 -0300

    asdf

--- src/components/Custom8M-2023/data.jsx
+++ src/components/Custom8M-2023/data.jsx
@@ -110,10 +110,9 @@ const data = [
       'https://www.latercera.com/resizer/Qu46P89ir4atWf533gXiWavR4Zk=/900x600/smart/cloudfront-us-east-1.images.arcpublishing.com/copesa/3H2LFSNLVJHOZODI67U4ZEC4Q4.jpg',
     note: {
       title:
-        'Azzarth Maveth, la conductora del Transantiago que es popular en redes sociales: “Quizás nací para manejar”',
-      description: 'Por Valentina González C. / Foto: Alejandra González',
+        'Azzarth Maveth, la conductora del Transantiago que es popular en redes sociales: “En el hogar del Sename donde crecí, conocí a mujeres que me ayudaron mucho”',
       description:
-        'Dentro de la jornada laboral de Azzarth Maveth (27) como conductora del Transantiago hay saludos de los pasajeros, pero también peticiones de fotos. “La primera vez que me preguntaron si se podían tomar una foto conmigo me pareció muy raro”, cuenta Azzarth, quien en TikTok suma más de 97 mil seguidores. En la plataforma comparte su día a día como conductora de transporte público a través de videos de humor y también respondiendo dudas de la gente.',
+        'Dentro de la jornada laboral de Azzarth Maveth (27) como conductora del Transantiago hay saludos de los pasajeros, pero también peticiones de fotos. “La primera vez que me preguntaron si se podían tomar una foto conmigo me pareció muy raro”, cuenta Azzarth, quien en TikTok suma más de 97 mil seguidores.',
       imgurl: '',
       url: 'https://www.latercera.com/paula/azzarth-maveth-la-conductora-del-transantiago-que-es-popular-en-redes-sociales-quizas-naci-para-manejar/',
       date: '',
