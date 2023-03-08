ec15f8d6c2981dc942111bc0b2aeb3d529912525
Ålex Acuña Viera <kyuumeitai@gmail.com>
2023-03-08 10:36:48 -0300

    update notas

--- src/components/Custom8M-2023/data.jsx
+++ src/components/Custom8M-2023/data.jsx
@@ -3,15 +3,75 @@ const data = [
     title: '',
     description: '',
     imgurl:
-      'https://www.latercera.com/resizer/Qu46P89ir4atWf533gXiWavR4Zk=/900x600/smart/cloudfront-us-east-1.images.arcpublishing.com/copesa/3H2LFSNLVJHOZODI67U4ZEC4Q4.jpg',
+      'https://www.latercera.com/resizer/Kv7TRoYaFDJGjyL89i2nPJkAoT0=/900x600/smart/cloudfront-us-east-1.images.arcpublishing.com/copesa/IO4OGG3VUNDATKNQUQV6Q5WFNM.jpg',
     note: {
       title:
-        'Azzarth Maveth, la conductora del Transantiago que es popular en redes sociales: “Quizás nací para manejar”',
-      description: 'Por Valentina González C. / Foto: Alejandra González',
+        'Cómo el encuentro entre dos mujeres potenció su lucha conjunta por las cuidadoras de Chile',
       description:
-        'Dentro de la jornada laboral de Azzarth Maveth (27) como conductora del Transantiago hay saludos de los pasajeros, pero también peticiones de fotos. “La primera vez que me preguntaron si se podían tomar una foto conmigo me pareció muy raro”, cuenta Azzarth, quien en TikTok suma más de 97 mil seguidores. En la plataforma comparte su día a día como conductora de transporte público a través de videos de humor y también respondiendo dudas de la gente.',
+        'Gloria Sepúlveda sabe, desde hace tiempo, lo que significa cuidar de otra persona. Primero lo hizo con su padre. Luego, con su madre. Después, con su hermana Francisca, solo tres años menor que ella.',
       imgurl: '',
-      url: 'https://www.latercera.com/paula/azzarth-maveth-la-conductora-del-transantiago-que-es-popular-en-redes-sociales-quizas-naci-para-manejar/',
+      url: 'https://www.latercera.com/paula/como-el-encuentro-entre-dos-mujeres-potencio-su-lucha-conjunta-por-las-cuidadoras-de-chile/',
+      date: '',
+    },
+  },
+
+  {
+    title: '',
+    description: '',
+    imgurl:
+      'https://www.latercera.com/resizer/vgnCFyM7Yd8U1ULjs4hryjlbr7g=/900x600/smart/cloudfront-us-east-1.images.arcpublishing.com/copesa/VONH6A2PVZHG7AINE3IMFJFWTA.jpg',
+    note: {
+      title:
+        'Por qué las mujeres necesitamos de otras mujeres: el boom de las mentoras',
+      description:
+        '¿Cómo avanzamos las mujeres? Juntas. Porque si históricamente hemos estado en desventaja frente al género masculino, y si recién hace 100 años pudimos acceder al espacio público, hoy damos día a día pasos agigantados, de la mano de otras mujeres.',
+      imgurl: '',
+      url: 'https://www.latercera.com/paula/por-que-las-mujeres-necesitamos-de-otras-mujeres-el-boom-de-las-mentoras/',
+      date: '',
+    },
+  },
+  {
+    title: '',
+    description: '',
+    imgurl:
+      'https://www.latercera.com/resizer/KAk3WmW5omqbxobPvHtDiAjdfuM=/900x600/smart/cloudfront-us-east-1.images.arcpublishing.com/copesa/Q53OVMB6FNCKVJEKV6LUQEB2CI.jpg',
+    note: {
+      title:
+        'Lorena Gallardo, CEO de Fundadoras: “Cuando una mujer que gana buen dinero lo reinvierte, hace crecer todo a su alrededor: la economía local y la de otras personas”',
+      description:
+        'Lorena Gallardo (36) es psicóloga organizacional y online business coach. Creó Fundadoras, una escuela de negocios que busca el empoderamiento económico de las mujeres empresarias. Un lugar donde no sólo hablan de números y estrategias, sino también de emociones y liderazgo.',
+      imgurl: '',
+      url: 'https://www.latercera.com/paula/lorena-gallardo-ceo-de-fundadoras-cuando-una-mujer-que-gana-buen-dinero-lo-reinvierte-hace-crecer-todo-a-su-alrededor-la-economia-local-y-la-de-otras-personas/',
+      date: '',
+    },
+  },
+
+  {
+    title: '',
+    description: '',
+    imgurl:
+      'https://www.latercera.com/resizer/_s5-g8lsqGhG0Y9oH0AiPJIBNvo=/900x600/smart/cloudfront-us-east-1.images.arcpublishing.com/copesa/PTQDZGARLZHJTGGO2ICTUIE3RY.jpg',
+    note: {
+      title: 'Mujeres tejiendo redes',
+      description:
+        'Diana Choque (28) teje desde que tiene memoria. Tal como le enseñó su abuela, utiliza telares artesanales de dos y cuatro pedales para crear diseños en lanas de alpaca. Esta actividad, característica de su pueblo aymara, no solo ha sido una forma de ganarse la vida en la comuna de Huara, en el Norte Grande, sino también de preservar una cultura que sus a ojos se ha ido perdiendo con los años.',
+      imgurl: '',
+      url: 'https://www.latercera.com/paula/mujeres-tejiendo-redes/',
+      date: '',
+    },
+  },
+  {
+    title: '',
+    description: '',
+    imgurl:
+      'https://www.latercera.com/resizer/YkLovZsAoiXUctQBxrLLhaFVfgI=/900x600/smart/cloudfront-us-east-1.images.arcpublishing.com/copesa/ESISSUU7L5A77F2AL4EGXKQVOQ.jpg',
+    note: {
+      title:
+        'Sharoni Rosenberg y su camino acompañando a mujeres a encontrar el propósito',
+      description:
+        'Antes de sus 33 años, la abogada y actual conferencista Sharoni Rosenberg – fundadora de la consultora Purposely y autora del libro El propósito no era lo que yo creía: pero en el camino descubrí mucho más- era lo que siempre ella había considerado como una mujer ‘exitosa’: una hija preocupada, una madre atenta con su familia, una buena esposa y una persona reconocida en su trabajo.',
+      imgurl: '',
+      url: 'https://www.latercera.com/paula/sharoni-rosenberg-y-su-camino-acompanando-a-mujeres-a-encontrar-el-proposito/',
       date: '',
     },
   },
@@ -33,14 +93,29 @@ const data = [
     title: '',
     description: '',
     imgurl:
-      'https://www.latercera.com/resizer/YkLovZsAoiXUctQBxrLLhaFVfgI=/900x600/smart/cloudfront-us-east-1.images.arcpublishing.com/copesa/ESISSUU7L5A77F2AL4EGXKQVOQ.jpg',
+      'https://www.latercera.com/resizer/NR3Zc3dUj0W0xU_4jxIsRmWgXO8=/900x600/smart/cloudfront-us-east-1.images.arcpublishing.com/copesa/6TM6H7UU6JASVASQVCWRGHDTEQ.jpg',
+    note: {
+      title: 'La mentoría que me cambió la vida',
+      description:
+        'María José López (29) y Lissette Latin son hoy buenas amigas. Hablan casi todos los días por Whatsapp e incluso tienen algunos proyectos juntas en relación a un tema que las unió profundamente: los hijos con una discapacidad.',
+      imgurl: '',
+      url: 'https://www.latercera.com/paula/la-mentoria-que-me-cambio-la-vida/',
+      date: '',
+    },
+  },
+  {
+    title: '',
+    description: '',
+    imgurl:
+      'https://www.latercera.com/resizer/Qu46P89ir4atWf533gXiWavR4Zk=/900x600/smart/cloudfront-us-east-1.images.arcpublishing.com/copesa/3H2LFSNLVJHOZODI67U4ZEC4Q4.jpg',
     note: {
       title:
-        'Sharoni Rosenberg y su camino acompañando a mujeres a encontrar el propósito',
+        'Azzarth Maveth, la conductora del Transantiago que es popular en redes sociales: “Quizás nací para manejar”',
+      description: 'Por Valentina González C. / Foto: Alejandra González',
       description:
-        'Antes de sus 33 años, la abogada y actual conferencista Sharoni Rosenberg – fundadora de la consultora Purposely y autora del libro El propósito no era lo que yo creía: pero en el camino descubrí mucho más- era lo que siempre ella había considerado como una mujer ‘exitosa’: una hija preocupada, una madre atenta con su familia, una buena esposa y una persona reconocida en su trabajo.',
+        'Dentro de la jornada laboral de Azzarth Maveth (27) como conductora del Transantiago hay saludos de los pasajeros, pero también peticiones de fotos. “La primera vez que me preguntaron si se podían tomar una foto conmigo me pareció muy raro”, cuenta Azzarth, quien en TikTok suma más de 97 mil seguidores. En la plataforma comparte su día a día como conductora de transporte público a través de videos de humor y también respondiendo dudas de la gente.',
       imgurl: '',
-      url: 'https://www.latercera.com/paula/sharoni-rosenberg-y-su-camino-acompanando-a-mujeres-a-encontrar-el-proposito/',
+      url: 'https://www.latercera.com/paula/azzarth-maveth-la-conductora-del-transantiago-que-es-popular-en-redes-sociales-quizas-naci-para-manejar/',
       date: '',
     },
   },
